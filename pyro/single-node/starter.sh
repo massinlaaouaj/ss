ddbced2f427744c342dd25311d23363e6af98e72
Massin Laaouaj <massin.laaouaj@gmail.com>
2025-05-11 19:20:31 +0200

    pyro-improvements

--- pyro/single-node/starter.sh
+++ pyro/single-node/starter.sh
@@ -21,25 +21,25 @@ if [ $? -ne 0 ]; then
 fi
 
 # Insult services:
-python3 launcher.py 500 0
-python3 launcher.py 800 0
-python3 launcher.py 1000 0
-python3 launcher.py 1500 0
-python3 launcher.py 2500 0
-python3 launcher.py 4000 0
-python3 launcher.py 5000 0
-python3 launcher.py 10000 0
-python3 launcher.py 15000 0
-python3 launcher.py 20000 0
+python3 launcherTests.py 500 0
+python3 launcherTests.py 800 0
+python3 launcherTests.py 1000 0
+python3 launcherTests.py 1500 0
+python3 launcherTests.py 2500 0
+python3 launcherTests.py 4000 0
+python3 launcherTests.py 5000 0
+python3 launcherTests.py 10000 0
+python3 launcherTests.py 15000 0
+python3 launcherTests.py 20000 0
 
 # Insult filers services:
-python3 launcher.py 500 500
-python3 launcher.py 800 800
-python3 launcher.py 1000 1000
-python3 launcher.py 1500 1500
-python3 launcher.py 2500 2500
-python3 launcher.py 4000 4000
-python3 launcher.py 5000 5000
-python3 launcher.py 10000 10000
-python3 launcher.py 15000 15000
-python3 launcher.py 20000 20000
+python3 launcherTests.py 500 500
+python3 launcherTests.py 800 800
+python3 launcherTests.py 1000 1000
+python3 launcherTests.py 1500 1500
+python3 launcherTests.py 2500 2500
+python3 launcherTests.py 4000 4000
+python3 launcherTests.py 5000 5000
+python3 launcherTests.py 10000 10000
+python3 launcherTests.py 15000 15000
+python3 launcherTests.py 20000 20000
